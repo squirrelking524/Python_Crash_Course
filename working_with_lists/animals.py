043af6abcc4848b55e979a93103a63c4b063dc10
David <davidlsmith524@gmail.com>
2025-09-27 07:37:14 -0400

    Looped through list of common animals

---
+++ working_with_lists/animals.py
@@ -0,0 +1,8 @@
+# List of 3 different animals with common characteristics
+common_animals = ["dog", "cat", "bunny"]
+
+for animal in common_animals:
+    print(f"A {animal} would make a great pet.")
+
+print(f"Each animal: {", ".join(common_animals)}, would make for a great pet.")
+print("Any of these animals would make for a great pet.")
