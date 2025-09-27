c6f9e0eb668e73bb49cd2d097c3b378c52875135
David <davidlsmith524@gmail.com>
2025-09-27 07:30:56 -0400

    added my fav pizzas and looped through them

---
+++ working_with_lists/pizzas.py
@@ -0,0 +1,10 @@
+# list of my 3 favorite pizzas
+fav_pizzas = ["pepperoni", "meat lovers", "bbq chicken"]
+
+'''for pizza in fav_pizzas:
+    print(f"I really like {pizza} pizza")'''
+
+print(f"I really love pizza. My favorite pizza of all is {fav_pizzas[0]} pizza.")
+print(f"My second, usually go to pizza, is {fav_pizzas[1]} pizza.")
+print(f"Lastly, when I'm in the mood, I go for {fav_pizzas[2]} pizza. I really love pizza.")
+
