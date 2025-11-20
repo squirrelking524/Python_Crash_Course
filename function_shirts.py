7687a28b3693f9379400bfd97e74cf61ac018920
David <davidlsmith524@gmail.com>
2025-11-20 16:40:24 -0500

    Created function to pass positionally and keywordly.

---
+++ function_shirts.py
@@ -0,0 +1,5 @@
+def make_shirt(size, text):
+    print(f"The shirt size is: {size}; and the text displayed: {text}")
+
+make_shirt("Medium", "Lover of All")
+make_shirt(size="Large", text="Hater of All")
