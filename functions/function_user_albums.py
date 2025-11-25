8676a3ecbe038904b86c64fb8bbadab9aa2ef86c
David <davidlsmith524@gmail.com>
2025-11-25 17:57:35 -0500

    added a break in the loop and titled name and title

--- functions/function_user_albums.py
+++ functions/function_user_albums.py
@@ -1,11 +1,16 @@
 def make_album(name, title):
-    record = f"Artist name:{name}; Album Title:{title}"
+    record = f"Artist name: {name.title()}; Album Title: {title.title()}"
     return record
 
 while True:
     print("\nWhat is your favorite musical artist and album title?")
+    print("(enter 'q' at any time to quit)")
     a_name = input("Artist name: ")
+    if a_name == 'q':
+        break
     a_title = input("Album title: ")
+    if a_title == 'q':
+        break
 
     album = make_album(a_name, a_title)
     print(f"\nYour favorite artist and album is: {album}")
