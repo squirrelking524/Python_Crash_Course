3e86b82f9442116e4fcf6e88e81ae2a8c4dad9ad
David <davidlsmith524@gmail.com>
2025-12-08 17:55:23 -0500

    Changed previous excercise 8-9 to not change original list

---
+++ functions/function_archived_messages.py
@@ -0,0 +1,18 @@
+def show_messages(text_msgs, sent_msgs):
+    while text_msgs:
+        text = text_msgs.pop()
+        print(f"Your text message says, '{text}.'")
+        sent_msgs.append(text)
+
+def send_messages(sent_msgs):
+    print("\nThe following text messages have been sent:")
+    for sent_msg in sent_msgs:
+        print(sent_msg)
+
+text_msgs = ['Hey, r u there?', 'when u comin?', 'whats up bro?']
+sent_msgs = []
+
+show_messages(text_msgs[:], sent_msgs)
+send_messages(sent_msgs)
+
+print(text_msgs)
