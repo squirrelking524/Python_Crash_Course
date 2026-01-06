4dfeede27ec075d5a6068e0aa2de562a1d89fddd
squirrelking524 <davidlsmith524@gmail.com>
2026-01-05 19:55:15 -0500

    added set_number_served method and practiced printing and calling class

---
+++ classes/number_served.py
@@ -0,0 +1,28 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name.title()
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} serves wonderfully made {self.cuisine_type}")
+    
+    def open_restaurant(self):
+        print("The restaurant is open.")
+
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+
+restaurant = Restaurant('uncle beths', 'bbq')
+restaurant.describe_restaurant()
+
+print(f"Number of customers served: {restaurant.number_served}")
+restaurant.number_served = 200
+print(f"Number of customers served: {restaurant.number_served}")
+
+restaurant.number_served = 500
+print(f"Number of customers served: {restaurant.number_served}")
+
+restaurant.set_number_served(300)
+print(f"Number of customers served: {restaurant.number_served}")
