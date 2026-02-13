90eb2c34255f38c1bf0787bf1bbb64f58b7200e9
David <davidlsmith524@gmail.com>
2026-02-13 08:41:13 -0500

    created a lottery ticket generator that either can allow for repeating values or doesn't allow repeating values

---
+++ classes/lottery.py
@@ -0,0 +1,20 @@
+# generate lottery ticket
+
+from random import choice
+
+lottery_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
+
+winning_ticket = []
+print("Today's winning lottery ticket is...")
+
+while len(winning_ticket) < 4:
+    pulled_item = choice(lottery_values)
+    winning_ticket.append(pulled_item)
+
+"""
+    if pulled_item not in winning_ticket:
+        print(f" We pulled a {pulled_item}!")
+        winning_ticket.append(pulled_item)
+        """
+
+print(f"The final winning ticket is: {winning_ticket}")
