16ed4829aa1221c62c9381cd590efad1ad280358
squirrelking524 <davidlsmith524@gmail.com>
2026-02-22 10:08:14 -0500

    created a function to print the name of pets and raised FileNotFoundError if not found

---
+++ files_and_exceptions/cats_and_dogs.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def pet_names(filename):
+    try:
+        contents = path.read_text()
+    except:
+        print(f"Unfortunately, we can't find {path}")
+    else:
+        print(f"These are the pet names: \n{contents.rstrip()}")
+
+files = ['dogs.txt', 'cats.txt']
+
+for filename in files:
+    path = Path(filename)
+    pet_names(path)
+
+
