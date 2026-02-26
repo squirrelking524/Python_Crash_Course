07dc20b2a3c83e7b6e81103e29c981c91b43e58c
David <davidlsmith524@gmail.com>
2026-02-25 19:29:22 -0500

    tried doing the code more by myself, created a function of correct user

---
+++ files_and_exceptions/verify_user.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import json
+
+path = Path('username.json')
+def get_stored_user(path):
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+    
+def get_new_user(path):
+    username = input("What is your username? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
+    
+def correct_user(path):    
+    username = get_stored_user(path)
+    question_user = input(f"Is this your correct username: {username} (y/n)? ")
+    if question_user.lower() == 'y':
+        greet_user()
+    else:
+        username = get_new_user(path)
+
+def greet_user():
+    username = get_stored_user(path)
+    if username:
+        print(f"Welcome back, {username}")
+    else:
+        username = get_new_user(path)
+        print(f"We will remember you next time you are back, {username}")
+
+correct_user(path)
