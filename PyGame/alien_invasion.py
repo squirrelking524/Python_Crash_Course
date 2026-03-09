7bde5ef29e08be77f1ed3e076b33c468fc424a29
David <davidlsmith524@gmail.com>
2026-03-08 20:14:16 -0400

    added KEYDOWN and KEYUP event types as well as update function from Ship module

--- PyGame/alien_invasion.py
+++ PyGame/alien_invasion.py
@@ -5,7 +5,7 @@ from settings import Settings
 from ship import Ship
 
 class AlienInvasion:
-    """OVerall class to manage game assets and behavior"""
+    """Overall class to manage game assets and behavior"""
 
     def __init__(self):
         """Initialize the game, and create game resources"""
@@ -23,18 +23,27 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)  
+    
+    def _check_events(self):
+        # Watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                     if event.key == pygame.K_RIGHT:
+                          #Move the ship to the right
+                          self.ship.rect.x += 1
 
-            # Redraw screen during each pass through the loop
+    def _update_screen(self):
+         # Redraw screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
-            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
