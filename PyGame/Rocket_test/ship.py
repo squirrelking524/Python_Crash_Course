0c63ed6fdcc2e492322d761e3314416d10164ac0
David <davidlsmith524@gmail.com>
2026-03-17 08:55:34 -0400

    updated image file by pointing to Rocket_test directory

--- PyGame/Rocket_test/ship.py
+++ PyGame/Rocket_test/ship.py
@@ -7,11 +7,26 @@ class Ship:
         self.screen_rect = rocket_game.screen.get_rect()
 
         # Load image and get its rect
-        self.image = pygame.image.load('images/onlyrocket.bmp')
+        self.image = pygame.image.load('Rocket_test/images/onlyrocket.bmp')
         self.rect = self.image.get_rect()
 
         # Start ship in center
         self.rect.center = self.screen_rect.center
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_down = False
+        self.moving_up = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
+        elif self.moving_down:
+            self.rect.x += 1
+        elif self.moving_up:
+            self.rect.x -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
