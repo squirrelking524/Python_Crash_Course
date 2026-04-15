d3c225fa68ea081638acc32648e556112d782edb
David <davidlsmith524@gmail.com>
2026-04-15 16:59:23 -0400

    limited player to 3 bullets on the screen

--- PyGame/Alien_Invasion/settings.py
+++ PyGame/Alien_Invasion/settings.py
@@ -4,6 +4,7 @@ class Settings:
     def __init__(self):
         """Initialize games settings"""
         # Screen settings
+        # Changed width and height from 1200 and 800
         self.screen_width = 800 
         self.screen_height = 600
         self.bg_color = (20, 20, 50)
@@ -16,3 +17,4 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
