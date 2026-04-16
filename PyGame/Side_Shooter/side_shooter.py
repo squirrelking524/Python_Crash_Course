cf7165046afc80bf4832b2dc358aad5acda4d790
David <davidlsmith524@gmail.com>
2026-04-15 20:05:36 -0400

    Added sys settings, imported Settings, made init, run game, check events and update screen methods

--- PyGame/Side_Shooter/side_shooter.py
+++ PyGame/Side_Shooter/side_shooter.py
@@ -1,5 +1,40 @@
 import sys
 import pygame
+from settings import Settings
+
 
 class SideShooter:
     """Manage game assest and behavior"""
+
+    def __init__(self):
+        """Init game and create resources"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Side Shooter")
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+
+        pygame.display.flip()
+
+if __name__ == '__main__':
+        ss = SideShooter()
+        ss.run_game()
