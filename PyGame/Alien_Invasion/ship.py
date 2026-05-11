5a9f81740fa89d6677de897924afdf1ddd07d7bc
david-smith <davidlsmith524@gmail.com>
2026-05-11 16:11:25 -0400

    fixed ship image

--- PyGame/Alien_Invasion/ship.py
+++ PyGame/Alien_Invasion/ship.py
@@ -10,7 +10,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get it rect
-        self.image = pygame.image.load('/home/dls013/repos/Python/Python_Crash_Course/PyGame/Alien_Invasion/images/DurrrSpaceShip.bmp')
+        self.image = pygame.image.load('/home/david-smith/repos/Python/Python_Crash_Course/PyGame/Alien_Invasion/images/DurrrSpaceShip.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen
